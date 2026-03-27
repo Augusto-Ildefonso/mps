818df2dc7bcc1c906186bf9528b6a5b1c13ec252
Augusto-Ildefonso <afildefonso16@gmail.com>
2026-03-27 11:08:59 -0300

    feat: carousel component

--- src/component/CarouselCard/CarouselCard.jsx
+++ src/component/CarouselCard/CarouselCard.jsx
@@ -2,8 +2,18 @@ import React from "react"
 
 const CarouselCard = (props) => {
     return(
-        <div>
+        <div className="p-0 w-full h-50 bg-deep-blue rounded-xl flex flex-row items-center justify-between relative overflow-hidden shadow-2xl">
+            <div className="w-1/2 h-full text-white flex flex-col items-start justify-center px-4 gap-2">
+                <p className="font-extrabold text-balance">Encontre peças genuínas. Dirija com confiança.</p>
+                <button className="w-auto h-10 p-2 rounded bg-orange">
+                    <p className="font-bold">Compre Agora</p>
+                </button>
 
+            </div>
+            <div className="w-1/2 h-full flex items-center justify-end">
+                <img src={props.img} alt={props.alt} className="h-full w-full object-cover object-right"/>
+            </div>
+            
         </div>
     );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { LuAlignJustify, LuShoppingCart } from "react-icons/lu"
 import logo from "../assets/logo.jpeg"
-import CarouselCard from "../component/CarouselCard/CarouselCard"
+import motor from "../assets/motor.jpg"
+import Carousel from "../component/carousel/Carousel"
+
 
 const HomePage = () => {
     return(
-        <div>
+        <div className="grid grid-rows-4 h-full w-auto p-2">
             <header className="flex items-center gap-4 px-4 py-2">
                 <LuAlignJustify className="text-3xl shrink-0 stroke-deep-blue active:stroke-light-blue"/>
                 <div className="flex-1 flex justify-center">
@@ -13,8 +15,8 @@ const HomePage = () => {
                 </div>
                 <LuShoppingCart className="text-3xl shrink-0 stroke-deep-blue active:stroke-light-blue"/>
             </header>
-            <section className="">
-                <CarouselCard image="../assets/motor.jpg" className/>
+            <section className="flex justify-center items-center">
+                <Carousel imagesUrl={[motor, motor]} imagesAlt={["Motor", "Motor"]}/>
             </section>
         </div>
     );
