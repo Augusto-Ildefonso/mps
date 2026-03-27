2d57c3690d18944412fb481bc78c16fedb94070c
Augusto-Ildefonso <afildefonso16@gmail.com>
2026-03-27 11:21:42 -0300

    fix: font size

--- src/component/CarouselCard/CarouselCard.jsx
+++ src/component/CarouselCard/CarouselCard.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
 const CarouselCard = (props) => {
+    
     return(
         <div className="p-0 w-full h-50 bg-deep-blue rounded-xl flex flex-row items-center justify-between relative overflow-hidden shadow-2xl">
             <div className="w-1/2 h-full text-white flex flex-col items-start justify-center px-4 gap-2">
-                <p className="font-extrabold text-balance">Encontre peças genuínas. Dirija com confiança.</p>
+                <p className="text-xl font-extrabold text-balance">Conheça mais nossos produtos.</p>
                 <button className="w-auto h-10 p-2 rounded bg-orange">
                     <p className="font-bold">Compre Agora</p>
                 </button>
