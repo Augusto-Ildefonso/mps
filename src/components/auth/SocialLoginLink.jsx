c55a7d092a9cc26a1c10708b2e5c452bfb32f747
Marco Túlio <marcotulio2709@usp.br>
2026-03-30 20:22:14 -0300

    Fix some parameters and css class

--- src/components/auth/SocialLoginLink.jsx
+++ src/components/auth/SocialLoginLink.jsx
@@ -4,7 +4,7 @@ const SocialLoginLink = ({ href = '#', text = 'Login pelo Google', icon = <FcGoo
   return (
     <a
       href={href}
-      className="flex h-8 w-full items-center justify-center gap-3 rounded-md border border-deep-blue bg-white text-sm font-medium text-slate-700 transition hover:border-slate-400 hover:bg-slate-50"
+      className="flex h-12 w-full items-center justify-center gap-3 rounded-md border border-deep-blue bg-white text-sm font-medium text-slate-700 transition hover:border-slate-400 hover:bg-slate-50"
     >
       {icon}
       <span>{text}</span>
