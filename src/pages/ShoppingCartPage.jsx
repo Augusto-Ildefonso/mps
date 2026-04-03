5c143b4b88f53dcd2721656246acd710a8f3f3af
Augusto-Ildefonso <afildefonso16@gmail.com>
2026-04-03 16:44:07 -0300

    update: cart page

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,83 +1,13 @@
 import { useState } from "react"
-import { LuArrowLeft } from "react-icons/lu"
 import { useNavigate } from "react-router-dom"
 import motor from "../assets/motor.jpg"
+import NavBar from "../component/NavBar/NavBar"
 import ProductShoppingCartCard from "../component/ProductShoppingCartCard/ProductShoppingCartCard"
-
-const mockReq = [
-    {
-        "id": 1,
-        "name": "Motor",
-        "price": 2000.0,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 2,
-        "name": "Motor",
-        "price": 5000.0,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 3,
-        "name": "Motor",
-        "price": 100.0,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 4,
-        "name": "Motor",
-        "price": 404.0,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 5,
-        "name": "Motor",
-        "price": 599.99,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 6,
-        "name": "Motor",
-        "price": 10.0,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 7,
-        "name": "Motor",
-        "price": 599.99,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-    {
-        "id": 8,
-        "name": "Motor",
-        "price": 10.0,
-        "url": motor,
-        "alt": "motor",
-        "number": 1,
-    },
-]
+import mockReq from "../mock"
 
 const ShoppingCartPage = () => {
     const [cartItems, setCartItems] = useState(mockReq)
     const navigate = useNavigate()
-
-    const handleBack = () => {
-        navigate(-1)
-    }
 
     const handleCheckout = () => {
         // Lógica do checkout
@@ -107,25 +37,17 @@ const ShoppingCartPage = () => {
     }
 
     return(
-        <div>
-            <header className="grid grid-cols-3 items-center px-4 py-2 shadow">
-                <button
-                    type="button"
-                    aria-label="Voltar"
-                    onClick={handleBack}
-                    className="justify-self-start inline-flex items-center justify-center text-3xl shrink-0 text-deep-blue"
-                >
-                    <LuArrowLeft className="shrink-0 stroke-current active:text-light-blue transition duration-300 active:scale-95  " />
-                </button>
+        <div className="h-[100dvh] flex flex-col overflow-hidden w-full">
+            <header className="grid grid-cols-1 items-center px-4 py-2 shadow shrink-0">
                 <div className="justify-self-center flex justify-center items-center">
                     <h1 className="font-bold text-2xl">Carrinho</h1>
                 </div>
             </header>
-            <div className="flex flex-col justify-center items-center gap-5">
-                <button className="w-1/3 p-2 bg-orange rounded-3xl mt-5 active:scale-95 duration" onClick={handleCheckout}>
+            <div className="flex flex-col items-center gap-5 flex-1 overflow-hidden px-4 pt-5">
+                <button className="w-1/3 p-2 bg-orange rounded-3xl active:scale-95 duration shrink-0" onClick={handleCheckout}>
                     <p>Finalizar pedido</p>
                 </button>
-                <div>
+                <div className="flex-1 w-full overflow-y-auto pb-20">
                     {cartItems.map((element) => (
                         <ProductShoppingCartCard
                             key={element.id}
@@ -141,7 +63,7 @@ const ShoppingCartPage = () => {
                     ))}
                 </div>
             </div>
-
+            <NavBar/>
         </div>
     )
 }
