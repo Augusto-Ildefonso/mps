f28210d8f096c9017f50bc0f61c17c6a359eadec
Augusto-Ildefonso <afildefonso16@gmail.com>
2026-04-03 12:26:53 -0300

    feat: product card handleClick

--- src/component/ProductCard/ProductCard.jsx
+++ src/component/ProductCard/ProductCard.jsx
@@ -1,8 +1,18 @@
-import { FaCartPlus } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa"
+import { useNavigate } from "react-router-dom"
 
 const ProductCard = (props) => {
+    const navigate = useNavigate()
+    
+    const handleCardClick = (productId) => {
+        navigate("/product")
+        //navigate(`/product/?${productId}`)
+    }
+
     return(
-        <div className="flex flex-col w-40 h-auto shadow-xl rounded-xl p-4 gap-y-1">
+        <div className="flex flex-col w-40 h-auto shadow-xl rounded-xl p-4 gap-y-1"
+        onClick={() => handleCardClick()}
+        >
             <div className="w-7/8 flex-1 flex justify-center items-center">
                 <img src={props.productImage} alt={props.imageAlt} className="w-auto object-contain"/>
             </div>
