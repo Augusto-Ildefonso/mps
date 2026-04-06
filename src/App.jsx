2818a718d26ca7caadb7deb5a1661677779351bc
Marco Tulio <marcotulio270903@gmail.com>
2026-04-06 11:03:54 -0300

    Feat: Address Pages with modify and add new address

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import ProductPage from './pages/ProductPage'
 import AccountPage from './pages/AccountPage'
 import ShoppingCartPage from './pages/ShoppingCartPage'
 import Orders from './pages/account/Orders'
-import AddressPage from './pages/account/AdressesPage'
-import {AddressEditPage} from './pages/account/AddressEditPage'
+import AddressPage from './pages/account/AddressesPage'
+import AddressEditPage from './pages/account/AddressEditPage'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import  AddressNewPage  from './pages/account/AddresNewPage'
 function App() {
   return (
     <>
@@ -20,6 +21,7 @@ function App() {
             <Route path="/account/orders" element={<Orders />}/>
             <Route path="/account/addresses" element={<AddressPage />}/>
             <Route path="/account/addresses/edit" element={<AddressEditPage />}/>
+            <Route path="/account/addresses/new" element={<AddressNewPage />}/>
             <Route path="/product" element={<ProductPage />}/>
           </Routes>
       </Router>
---
+++ src/pages/account/AddressesPage.jsx
@@ -0,0 +1,30 @@
+import AddressCard from "../../component/Address/AdressCard";
+import { LuPlus } from "react-icons/lu";
+import Header from "../../component/Header/Header";
+import { mockAddress } from "../../mock";
+import NavBar from "../../component/NavBar/NavBar";
+import  Button  from "../../component/GeneralButton/Button";
+import { useNavigate } from "react-router-dom";
+const AddressPage = ({addresses}) => {
+    const navigate = useNavigate();
+    const handleNavigation = (path) => {
+        const routes = {
+            "new": "/account/addresses/new"
+        };
+        navigate(routes[path] || "/account/addresses");
+    }
+    return (
+        <div className="address-page p-3">
+            <Header />
+            <h1 className="text-xl font-bold text-center text-deep-blue mb-3 ">Meus Endereços</h1>
+            <div className="flex flex-col gap-4">
+            {mockAddress.map((address, _) => (
+                <AddressCard address={address} />
+            ))}
+            </div>
+            <Button text="Adicionar Novo Endereço" bg_color="bg-orange" handleNavigation={() => {handleNavigation("new")}} icon={<LuPlus className="text-2xl" />} />
+            <NavBar />
+        </div>
+    )
+}
+export default AddressPage;
